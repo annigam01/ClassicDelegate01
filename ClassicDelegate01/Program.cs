1486c5ca3989ee2629373e96129070481f9af9bd
Anand Nigam <annigam01@hotmail.com>
2017-03-22 13:13:10 +0530

    Add project files.

---
+++ ClassicDelegate01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ClassicDelegate01
+{
+    public delegate int GetArea(int a);
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetArea A = CalculateAreaofSquare;
+            int finalarea = A(4);
+            Console.WriteLine(finalarea);
+            Console.ReadLine();
+            
+        }
+
+        public static int CalculateAreaofSquare(int a)
+        {
+            return a * a;
+        }
+    }
+}
